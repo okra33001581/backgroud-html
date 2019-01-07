3b488314523f23fbf74543b81738c5caa6f52b91
Your Name <you@example.com>
2019-01-07 14:52:22 +0800

    'update'

--- src/views/home/main.vue
+++ src/views/home/main.vue
@@ -1,84 +1,111 @@
 <template>
-    <div>
-        <div class="ms-doc">
-            <h3>README.md</h3>
-            <article>
-                <h1>vue-admin</h1>
-                <p>基于Vue.js 2.x系列 + Element UI + RBAC/AUTH权限 的后台管理系统</p>
-                <h2>前言</h2>
-                <p><strong>项目前端地址：</strong><a href="https://github.com/lmxdawn/vue-admin-html" target="_blank">https://github.com/lmxdawn/vue-admin-html</a></p>
-                <p><strong>项目JAVA后端地址：</strong><a href="https://github.com/lmxdawn/vue-admin-java" target="_blank">https://github.com/lmxdawn/vue-admin-java</a></p>
-                <p><strong>项目PHP后端地址：</strong><a href="https://github.com/lmxdawn/vue-admin-php" target="_blank">https://github.com/lmxdawn/vue-admin-php</a></p>
-                <p>欢迎大家的star</p>
-                <h2>功能</h2>
-                <el-checkbox disabled checked>管理员登录</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>登录</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>修改密码</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>角色管理</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>权限管理</el-checkbox>
-                <br>
-            </article>
-        </div>
+    <div class="dashboard-editor-container">
+
+        <github-corner style="position: absolute; top: 0px; border: 0; right: 0;"/>
+
+        <panel-group @handleSetLineChartData="handleSetLineChartData"/>
+
+        <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+            <line-chart :chart-data="lineChartData"/>
+        </el-row>
+
+        <el-row :gutter="32">
+            <el-col :xs="24" :sm="24" :lg="8">
+                <div class="chart-wrapper">
+                    <raddar-chart/>
+                </div>
+            </el-col>
+            <el-col :xs="24" :sm="24" :lg="8">
+                <div class="chart-wrapper">
+                    <pie-chart/>
+                </div>
+            </el-col>
+            <el-col :xs="24" :sm="24" :lg="8">
+                <div class="chart-wrapper">
+                    <bar-chart/>
+                </div>
+            </el-col>
+        </el-row>
+
+        <el-row :gutter="8">
+            <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
+                <transaction-table/>
+            </el-col>
+            <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+                <todo-list/>
+            </el-col>
+            <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+                <box-card/>
+            </el-col>
+        </el-row>
 
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {};
+    import GithubCorner from '@/components/GithubCorner'
+    import PanelGroup from './components/PanelGroup'
+    import LineChart from './components/LineChart'
+    import RaddarChart from './components/RaddarChart'
+    import PieChart from './components/PieChart'
+    import BarChart from './components/BarChart'
+    import TransactionTable from './components/TransactionTable'
+    import TodoList from './components/TodoList'
+    import BoxCard from './components/BoxCard'
+
+    const lineChartData = {
+        newVisitis: {
+            expectedData: [100, 120, 161, 134, 105, 160, 165],
+            actualData: [120, 82, 91, 154, 162, 140, 145]
+        },
+        messages: {
+            expectedData: [200, 192, 120, 144, 160, 130, 140],
+            actualData: [180, 160, 151, 106, 145, 150, 130]
+        },
+        purchases: {
+            expectedData: [80, 100, 121, 104, 105, 90, 100],
+            actualData: [120, 90, 100, 138, 142, 130, 130]
+        },
+        shoppings: {
+            expectedData: [130, 140, 141, 142, 145, 150, 160],
+            actualData: [120, 82, 91, 154, 162, 140, 130]
+        }
     }
-};
+
+    export default {
+        name: 'DashboardAdmin',
+        components: {
+            GithubCorner,
+            PanelGroup,
+            LineChart,
+            RaddarChart,
+            PieChart,
+            BarChart,
+            TransactionTable,
+            TodoList,
+            BoxCard
+        },
+        data() {
+            return {
+                lineChartData: lineChartData.newVisitis
+            }
+        },
+        methods: {
+            handleSetLineChartData(type) {
+                this.lineChartData = lineChartData[type]
+            }
+        }
+    }
 </script>
 
-<style scoped>
-.ms-doc {
-    width: 100%;
-    max-width: 980px;
-    font-family: PingFang SC, Helvetica Neue, Arial, sans-serif;
-    color: #2d2f33;
-}
-.ms-doc h3 {
-    padding: 9px 10px 10px;
-    margin: 0;
-    font-size: 14px;
-    line-height: 17px;
-    background-color: #f5f5f5;
-    border: 1px solid #d8d8d8;
-    border-bottom: 0;
-    border-radius: 3px 3px 0 0;
-}
-.ms-doc article {
-    padding: 45px;
-    word-wrap: break-word;
-    background-color: #fff;
-    border: 1px solid #ddd;
-    border-bottom-right-radius: 3px;
-    border-bottom-left-radius: 3px;
-}
-.ms-doc article h1 {
-    font-size: 32px;
-    padding-bottom: 10px;
-    margin-bottom: 15px;
-    border-bottom: 1px solid #ddd;
-}
-.ms-doc article h2 {
-    margin: 24px 0 16px;
-    font-weight: 600;
-    line-height: 1.25;
-    padding-bottom: 7px;
-    font-size: 24px;
-    border-bottom: 1px solid #eee;
-}
-.ms-doc article p {
-    margin-bottom: 15px;
-    line-height: 1.5;
-}
-.ms-doc article .el-checkbox {
-    margin-bottom: 5px;
-}
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .dashboard-editor-container {
+        padding: 32px;
+        background-color: rgb(240, 242, 245);
+        .chart-wrapper {
+            background: #fff;
+            padding: 16px 16px 0;
+            margin-bottom: 32px;
+        }
+    }
 </style>
