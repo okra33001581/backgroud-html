988b600e23d7c965b758b2bdceb05a34c3e896f1
Your Name <you@example.com>
2019-01-30 16:51:44 +0800

    'update'

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -38,6 +38,7 @@
                     </div>
                 </div>
                 <div class="header-tabs-box">
+
                     <el-breadcrumb class="app-levelbar" separator="/">
                         <el-breadcrumb-item v-for="(item,index)  in levelList" :key="item.path">
                         <span v-if='item.redirect==="noredirect"||index==levelList.length-1'
@@ -47,9 +48,60 @@
                     </el-breadcrumb>
                 </div>
 
+                <div class="header-tabs-middle">
+                    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                        <el-menu-item index="1"><img
+                                src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548848132312&di=6c64711d803871a0654003d53b70803c&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_origin_min_pic%2F01%2F53%2F29%2F265746caa127189.jpg"
+                                width="50px" height="30px"></img>
+                        </el-menu-item>
+                        <el-submenu index="2">
+                            <template slot="title"><img
+                                    src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548848132312&di=6c64711d803871a0654003d53b70803c&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_origin_min_pic%2F01%2F53%2F29%2F265746caa127189.jpg"
+                                    width="50px" height="30px"></img></template>
+                            <el-menu-item index="2-1">选项1</el-menu-item>
+                            <el-menu-item index="2-2">选项2</el-menu-item>
+                            <el-menu-item index="2-3">选项3</el-menu-item>
+                            <el-submenu index="2-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                                <el-menu-item index="2-4-3">选项3</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-menu-item index="3">消息中心</el-menu-item>
+                        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+                    </el-menu>
+                    <div class="line"></div>
+                    <el-menu
+                            :default-active="activeIndex2"
+                            class="el-menu-demo"
+                            mode="horizontal"
+                            @select="handleSelect"
+                            background-color="#545c64"
+                            text-color="#fff"
+                            active-text-color="#ffd04b">
+                        <el-menu-item index="1">处理中心</el-menu-item>
+                        <el-submenu index="2">
+                            <template slot="title">我的工作台</template>
+                            <el-menu-item index="2-1">选项1</el-menu-item>
+                            <el-menu-item index="2-2">选项2</el-menu-item>
+                            <el-menu-item index="2-3">选项3</el-menu-item>
+                            <el-submenu index="2-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                                <el-menu-item index="2-4-3">选项3</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+                        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+                    </el-menu>
+                </div>
+
+
                 <div class="header-right">
                     <!--<i class="el-icon-setting"></i>-->
-                    <i class="el-icon-setting" style="margin-right: 15px"></i>
+                    <!--<i class="el-icon-setting" style="margin-right: 15px"></i>-->
                     <el-dropdown trigger="click">
                         <!--<i class="el-icon-setting" style="margin-right: 15px"></i>-->
                         <span>{{username}}<i class="el-icon-arrow-down el-icon--right"></i></span>
@@ -401,6 +453,13 @@ export default {
     display: inline-block;
     height: $header-height;
     max-width: 60%;
+    overflow: hidden;
+}
+
+.header-tabs-middle {
+    display: inline-block;
+    height: $header-height;
+    max-width: 260%;
     overflow: hidden;
 }
 
