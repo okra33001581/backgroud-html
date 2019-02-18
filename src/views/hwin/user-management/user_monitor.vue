583283077c2b82a4a1f735fb9d6c99a5e7c47c83
Your Name <you@example.com>
2019-02-18 16:12:05 +0800

    update

--- src/views/hwin/user-management/user_monitor.vue
+++ src/views/hwin/user-management/user_monitor.vue
@@ -34,7 +34,7 @@
                 <el-button-group>
                     <el-button type="primary" icon="el-icon-refresh" @click="getList"></el-button>
                     <el-button type="primary" icon="el-icon-search" @click="onSubmit">查询</el-button>
-                    <el-button type="primary" icon="el-icon-plus" @click.native="handleForm(null,null)">新增</el-button>
+                    <!--<el-button type="primary" icon="el-icon-plus" @click.native="handleForm(null,null)">新增</el-button>-->
                 </el-button-group>
             </el-form-item>
         </el-form>
