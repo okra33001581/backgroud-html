d7235e00ca7c3c1eaceba69553b61e5d1962613c
okra33001581 <okra33001581@163.com>
2019-02-27 16:20:51 +0800

    update

--- src/views/hwin/site-management/information_list.vue
+++ src/views/hwin/site-management/information_list.vue
@@ -221,6 +221,27 @@
                         </quill-editor>
                     </el-row>
                 </template>
+
+                <div class="content-title">支持裁剪</div>
+                <div class="plugins-tips">
+                    vue-cropperjs：一个封装了 cropperjs 的 Vue 组件。
+                    访问地址：<a href="https://github.com/Agontuk/vue-cropperjs" target="_blank">vue-cropperjs</a>
+                </div>
+                <div class="crop-demo">
+                    <img :src="cropImg" class="pre-img">
+                    <div class="crop-demo-btn">选择图片
+                        <input class="crop-input" type="file" name="image" accept="image/*" @change="setImage"/>
+                    </div>
+                </div>
+
+                <el-dialog title="裁剪图片" :visible.sync="dialogVisible" width="30%">
+                    <vue-cropper ref='cropper' :src="imgSrc" :ready="cropImage" :zoom="cropImage" :cropmove="cropImage" style="width:100%;height:300px;"></vue-cropper>
+                    <span slot="footer" class="dialog-footer">
+                    <el-button @click="cancelCrop">取 消</el-button>
+                    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                </span>
+                </el-dialog>
+
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click.native="hideForm">取消</el-button>
@@ -243,6 +264,9 @@
     import 'quill/dist/quill.snow.css'
     import 'quill/dist/quill.bubble.css'
     import { quillEditor } from 'vue-quill-editor'
+
+    import VueCropper from 'vue-cropperjs'
+
 
 
     const formJson = {
@@ -283,6 +307,11 @@
                 },
                 tableKey: 0,
                 content: '',
+                defaultSrc: './site-management/img.jpg',
+                fileList: [],
+                imgSrc: '',
+                cropImg: '',
+                dialogVisible: false,
                 editorOption: {
                     placeholder: 'Hello World'
                 },
@@ -347,7 +376,8 @@
             };
         },
         components: {
-            quillEditor
+            quillEditor,
+            VueCropper
         },
         methods: {
             onSubmit() {
@@ -525,6 +555,35 @@
                     }
                 }))
             },
+            setImage(e) {
+                const file = e.target.files[0]
+                if (!file.type.includes('image/')) {
+                    return
+                }
+                const reader = new FileReader()
+                reader.onload = (event) => {
+                    this.dialogVisible = true
+                    this.imgSrc = event.target.result
+                    this.$refs.cropper && this.$refs.cropper.replace(event.target.result)
+                }
+                reader.readAsDataURL(file)
+            },
+            cropImage() {
+                this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL()
+            },
+            cancelCrop() {
+                this.dialogVisible = false
+                this.cropImg = this.defaultSrc
+            },
+            imageuploaded(res) {
+                console.log(res)
+            },
+            handleError() {
+                this.$notify.error({
+                    title: '上传失败',
+                    message: '图片上传接口上传失败，可更改为自己的服务器接口'
+                })
+            },
             formSubmit() {
                 this.$refs["dataForm"].validate(valid => {
                     if (valid) {
@@ -597,6 +656,7 @@
                 }
             }
         },
+
         filters: {
             statusFilterType(status) {
                 const statusMap = {
@@ -626,6 +686,7 @@
             this.getList();
             // 加载角色列表
             this.getRoleList();
+            this.cropImg = this.defaultSrc;
         }
     };
 </script>
@@ -635,5 +696,48 @@
 <style scoped>
     .editor-btn{
         margin-top: 20px;
+    }
+</style>
+
+<style scoped>
+    .content-title{
+        font-weight: 400;
+        line-height: 50px;
+        margin: 10px 0;
+        font-size: 22px;
+        color: #1f2f3d;
+    }
+    .pre-img{
+        width: 100px;
+        height: 100px;
+        background: #f8f8f8;
+        border: 1px solid #eee;
+        border-radius: 5px;
+    }
+    .crop-demo{
+        display: flex;
+        align-items: flex-end;
+    }
+    .crop-demo-btn{
+        position: relative;
+        width: 100px;
+        height: 40px;
+        line-height: 40px;
+        padding: 0 20px;
+        margin-left: 30px;
+        background-color: #409eff;
+        color: #fff;
+        font-size: 14px;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    .crop-input{
+        position: absolute;
+        width: 100px;
+        height: 40px;
+        left: 0;
+        top: 0;
+        opacity: 0;
+        cursor: pointer;
     }
 </style>
