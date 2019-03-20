66b44992373d51bda3445436c0302390b244b1a9
okra33001581 <okra33001581@163.com>
2019-03-20 11:05:33 +0800

    update

--- src/views/new/event/prize_audit.vue
+++ src/views/new/event/prize_audit.vue
@@ -91,7 +91,7 @@
                 highlight-current-row
                 style="width: 100%;"
                 @sort-change="sortChange"
-                element-loading-text="$t('page.loading')"
+                :element-loading-text="$t('page.loading')"
                 element-loading-spinner="el-icon-loading"
                 element-loading-background="rgba(0, 0, 0, 0.8)"
                 :header-cell-style="getRowClass"
