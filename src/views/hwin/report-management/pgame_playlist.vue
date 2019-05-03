d8b1fc6f354c9128b78f140666747824d032b1fe
okra33001581 <okra33001581@163.com>
2019-05-03 18:35:42 +0800

    update

--- src/views/hwin/report-management/pgame_playlist.vue
+++ src/views/hwin/report-management/pgame_playlist.vue
@@ -127,15 +127,13 @@
                         v-model="query.beginDate"
                         type="date"
                         placeholder="开始时间"
-                        value-format="yyyy-MM-dd 00:00:00" format="yyyy-MM-dd 00:00:00"
-                        :picker-options="pickerOptions0">
+                        value-format="yyyy-MM-dd 00:00:00" format="yyyy-MM-dd 00:00:00">
                 </el-date-picker>
                 <el-date-picker
                         v-model="query.endDate"
                         type="date"
                         placeholder="结束时间"
-                        value-format="yyyy-MM-dd 23:59:59" format="yyyy-MM-dd 23:59:59"
-                        :picker-options="pickerOptions1">
+                        value-format="yyyy-MM-dd 23:59:59" format="yyyy-MM-dd 23:59:59">
                 </el-date-picker>
             </el-form-item>
 
@@ -452,11 +450,11 @@
                             this.downloadLoading = false
                         })
 
-                        
+
                     })
                     .catch(() => {
                     });
-                
+
                 this.loading = false;
             },
             formatJson(filterVal, jsonData) {
